4e28cf5d09713dad60d7a45e81d96152ba2cdcff
Pedro Rodrigues <pr.rodriguesb@outlook.com>
2021-04-26 17:12:37 -0300

    alterações menores

--- semana4/funcoes/js-vanilla-template/src/index.js
+++ semana4/funcoes/js-vanilla-template/src/index.js
@@ -1,165 +1,165 @@
-/*
-Exercícios de interpretação
-1 -
-a. 
-10
-50 
+// /*
+// Exercícios de interpretação
+// 1 -
+// a. 
+// 10
+// 50 
 
-b. Seria impresso somente 50. O return faria com que fosse retornada a
-última variável salva na função.
+// b. Seria impresso somente 50. O return faria com que fosse retornada a
+// última variável salva na função.
 
-2 - 
-a.
-Darvas
-Caio
-
-b. 
-Amanda
-Caio
-
-3 - 
-Insere na array números pares multiplicados por si mesmos.
-Um bom nome para a função seria multipNumsPares.
-*/
-
-// Exercícios de escrita de código
-
-// 4 - 
-//a.
-function sobreMim() {
-    let nome = "Pedro Rodrigues"
-    let idade = "24 anos"
-    let estado = "São Paulo"
-    let profissao = "estudante"
-    console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${estado} e sou ${profissao}.`)
-
-} 
-
-sobreMim()
-
-// b.
-function sobreMim2(nome, idade, estado, estudante) {
-    if (estudante) {
-        estudanteOuNao = "sou"
-    } else {
-        estudanteOuNao = "não sou"
-    }
-    const frase = `Eu sou ${nome}, tenho ${idade} anos, moro em ${estado} e ${estudanteOuNao} estudante.`
-
-    return frase
-
-}
-
-const frase1 = sobreMim2("Pedro Rodrigues", 24, "São Paulo", true)
-console.log(frase1)
-
-// 5 -
+// 2 - 
 // a.
-function somaDoisNumeros(num1, num2) {
-    const soma = num1 + num2
-    return soma
-}
+// Darvas
+// Caio
 
-const resultado1 = somaDoisNumeros(2, 4)
-console.log(`A soma dá ${resultado1}.`)
+// b. 
+// Amanda
+// Caio
 
-// b.
-function comparacaoDoisNum(num1, num2) {
-    const comparacao = num1 >= num2
-    if (comparacao) {
-        maiorOuIgual = "maior que ou igual a"
-    } else {
-        maiorOuIgual = "menor que "
-    }
-    const frase = `${num1} é ${maiorOuIgual}o ${num2}.`
-    return frase
-}
+// 3 - 
+// Insere na array números pares multiplicados por si mesmos.
+// Um bom nome para a função seria multipNumsPares.
+// */
 
-const comparacao1 = comparacaoDoisNum(5, 6)
-console.log(comparacao1)
+// // Exercícios de escrita de código
 
-// c.
-function impressaoRepetida(frase) {
-    const fraseRepetida = String (frase + "\n").repeat(10) 
-    return fraseRepetida
-}
+// // 4 - 
+// //a.
+// function sobreMim() {
+//     let nome = "Pedro Rodrigues"
+//     let idade = "24 anos"
+//     let estado = "São Paulo"
+//     let profissao = "estudante"
+//     console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${estado} e sou ${profissao}.`)
 
-const frase1 = impressaoRepetida("Hasta la vista, baby!")
-console.log(frase1)
+// } 
 
-// 6 -
-// a.
-const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
-function quantidadeNumArray(array) {
-    const contagem = array.length
-    return contagem
+// sobreMim()
 
-}
-const contagem1 = quantidadeNumArray(array)
-console.log(contagem1)
+// // b.
+// function sobreMim2(nome, idade, estado, estudante) {
+//     if (estudante) {
+//         estudanteOuNao = "sou"
+//     } else {
+//         estudanteOuNao = "não sou"
+//     }
+//     const frase = `Eu sou ${nome}, tenho ${idade} anos, moro em ${estado} e ${estudanteOuNao} estudante.`
 
-// b.
-function numeroPar(num) {
-    const parOuNao = num % 2 === 0
+//     return frase
 
-    return parOuNao
-}
+// }
 
-const testePar1 = numeroPar(234)
-console.log(testePar1)
+// const frase1 = sobreMim2("Pedro Rodrigues", 24, "São Paulo", true)
+// console.log(frase1)
 
-// c.
+// // 5 -
+// // a.
+// function somaDoisNumeros(num1, num2) {
+//     const soma = num1 + num2
+//     return soma
+// }
 
-const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+// const resultado1 = somaDoisNumeros(2, 4)
+// console.log(`A soma dá ${resultado1}.`)
 
-function quantidadeNumPar(array) {
-    let contagem = 0
-    for (let numeros of array) {
-        const par = numeros % 2 === 0
-        if (par) {
-            contagem += 1
-        } 
-    }
-    return contagem
+// // b.
+// function comparacaoDoisNum(num1, num2) {
+//     const comparacao = num1 >= num2
+//     if (comparacao) {
+//         maiorOuIgual = "maior que ou igual a"
+//     } else {
+//         maiorOuIgual = "menor que "
+//     }
+//     const frase = `${num1} é ${maiorOuIgual}o ${num2}.`
+//     return frase
+// }
 
-}
-const contagem1 = quantidadeNumPar(array)
-console.log(contagem1)
+// const comparacao1 = comparacaoDoisNum(5, 6)
+// console.log(comparacao1)
 
-// d.
+// // c.
+// function impressaoRepetida(frase) {
+//     const fraseRepetida = String (frase + "\n").repeat(10) 
+//     return fraseRepetida
+// }
 
-const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+// const frase1 = impressaoRepetida("Hasta la vista, baby!")
+// console.log(frase1)
 
-function quantidadeNumPar(array) {
-    let contagem = 0
-    for (let numeros of array) {
-        const par = numeroPar(numeros)
-        if (par) {
-            contagem += 1
-        } 
-    }
-    return contagem
+// // 6 -
+// // a.
+// const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+// function quantidadeNumArray(array) {
+//     const contagem = array.length
+//     return contagem
 
-}
-const contagem1 = quantidadeNumPar(array)
-console.log(contagem1)
+// }
+// const contagem1 = quantidadeNumArray(array)
+// console.log(contagem1)
 
-// Desafios
+// // b.
+// function numeroPar(num) {
+//     const parOuNao = num % 2 === 0
 
-// 1.
+//     return parOuNao
+// }
 
-let arrowFunction1 = (parametro) => {
-    return console.log(parametro)
-}
+// const testePar1 = numeroPar(234)
+// console.log(testePar1)
 
-const testeArrowFunction1 = arrowFunction1("quitiulu")
+// // c.
 
-let arrowFunction2 = (num1, num2) => {
-    const soma = num1 + num2
-    arrowFunction1(soma)
-}
+// const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
 
-const testeArrowFunction2 = arrowFunction2(2, 2)
+// function quantidadeNumPar(array) {
+//     let contagem = 0
+//     for (let numeros of array) {
+//         const par = numeros % 2 === 0
+//         if (par) {
+//             contagem += 1
+//         } 
+//     }
+//     return contagem
+
+// }
+// const contagem1 = quantidadeNumPar(array)
+// console.log(contagem1)
+
+// // d.
+
+// const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+
+// function quantidadeNumPar(array) {
+//     let contagem = 0
+//     for (let numeros of array) {
+//         const par = numeroPar(numeros)
+//         if (par) {
+//             contagem += 1
+//         } 
+//     }
+//     return contagem
+
+// }
+// const contagem1 = quantidadeNumPar(array)
+// console.log(contagem1)
+
+// // Desafios
+
+// // 1.
+
+// let arrowFunction1 = (parametro) => {
+//     return console.log(parametro)
+// }
+
+// const testeArrowFunction1 = arrowFunction1("quitiulu")
+
+// let arrowFunction2 = (num1, num2) => {
+//     const soma = num1 + num2
+//     arrowFunction1(soma)
+// }
+
+// const testeArrowFunction2 = arrowFunction2(2, 2)
 
 // 2.
 // a.
@@ -211,8 +211,17 @@ console.log(filtroMaiorIdx1)
 
 // d.
 
+let arrayInvertida = []
+
 function inverterArray(array) {
-    return array.reverse();
+   let indiceArray = numeros.length - 1
+
+   while(indiceArray >= 0) {
+       arrayInvertida.push(numeros[indiceArray])
+       indiceArray -= 1;
+
+   }
+   return arrayInvertida
 }
 
 const inverterArray1 = inverterArray(numeros)
