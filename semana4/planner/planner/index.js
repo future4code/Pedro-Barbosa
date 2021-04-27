2494c78c7f3e3b94cf615c6e7278c2562498cd87
Pedro Rodrigues <pr.rodriguesb@outlook.com>
2021-04-27 16:30:38 -0300

    resolução desafios 2 ao 4

--- semana4/planner/planner/index.js
+++ semana4/planner/planner/index.js
@@ -1,14 +1,27 @@
+let cliques = 0
+
 function adicionarTarefa() {
     let diaDaSemana = document.getElementById("dias-semana").value
     let tarefaLista = document.getElementById("tarefa").value
+    let horarioDoDia = document.getElementById("horas-do-dia").value
 
     if (tarefaLista !== "") {
-        document.getElementById(diaDaSemana).innerHTML += `<li>${tarefaLista}</li>`
+        document.getElementById(diaDaSemana).innerHTML += `<li onclick='riscaTexto(this)'>${tarefaLista} às ${horarioDoDia}</li>`
+
         document.getElementById("tarefa").value = ""
     } else {
         alert("O campo está vazio! Por favor, insira uma tarefa.")
     }
-   
 
 }
 
+function riscaTexto(textoinserido){
+    textoinserido.style = "text-decoration: line-through";
+}
+
+function limparTudo() {
+    let listas = document.getElementsByTagName("li")
+    for (let limpeza of listas) {
+        limpeza.innerHTML = ""
+    }
+}
