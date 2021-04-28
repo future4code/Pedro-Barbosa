adb83c8d437f042926adee4770b149e60eb9785b
Pedro Rodrigues <pr.rodriguesb@outlook.com>
2021-04-28 17:26:22 -0300

    desafio incompleto

--- semana4/objetos/blog-boilerplate/index.js
+++ semana4/objetos/blog-boilerplate/index.js
@@ -4,6 +4,7 @@ function criaPostBlog() {
     let titulo = document.getElementById("titulo-post")
     let autor = document.getElementById("autor-post")
     let conteudo = document.getElementById("conteudo-post")
+    let imagem = document.getElementById("imagem-post")
 
     const postBlog = {
         titulo: titulo.value,
@@ -11,12 +12,27 @@ function criaPostBlog() {
         conteudo: conteudo.value
     }
 
-   let containerDePosts = document.getElementById("container-de-posts")
+let arrayObjPost = []
+    for (let objetos of Object.keys(postBlog)) {
+        arrayObjPost.push(postBlog[objetos])
+    }
+    console.log(arrayObjPost)
 
-    containerDePosts.innerHTML += `<h2>${postBlog.titulo} </h2> <h3> ${postBlog.autor} </h3> <p> ${postBlog.conteudo}</p>`
+    let containerDePosts = document.getElementById("container-de-posts")
 
+    if (imagem.value !== "") {
+        containerDePosts.innerHTML += `<h2>${postBlog.titulo} </h2> <h3> ${postBlog.autor} </h3> <img src="${imagem.value}"> <p> ${postBlog.conteudo}</p>`
+    } else {
+        containerDePosts.innerHTML += `<div> <h2>${postBlog.titulo} </h2> <h3> ${postBlog.autor} </h3> <p> ${postBlog.conteudo}</p> </div> `
+    }
+
+    
     titulo.value = ""
     autor.value = ""
     conteudo.value = ""
-    
-  }
+    imagem.value = ""
+
+}
+
+
+  
