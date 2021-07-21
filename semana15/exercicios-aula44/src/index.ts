cb51e40719c3cb0acb671f10899f64f04a493a66
IELSK <elementptz@hotmail.com>
2021-07-21 16:27:35 -0300

    inicio exercícios

--- semana15/exercicios-aula44/src/index.ts
+++ semana15/exercicios-aula44/src/index.ts
@@ -1,7 +1,7 @@
 import { app } from "./app"
 import { getAllUsers } from "./endpoints/getAllUsers"
 import { getUserByTypeOrName } from "./endpoints/getUserByTypeOrName"
-import { pingTest } from "./endpoints/PingTest"
+import { pingTest } from "./endpoints/pingTest"
 import { newUser } from "./endpoints/newUser"
 
 app.get("/ping", pingTest)
